76892758161286f434f480bd33736274c356fa3c
Ivan Milivojevic <seltik89@hotmail.com>
2023-09-23 19:07:39 +0200

    Starting setup with prettier added with formatting rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,4 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
+import { ChakraProvider, Box, Text, Link, VStack, Code, Grid, theme } from "@chakra-ui/react"
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
 import { Logo } from "./Logo"
 
--- src/ColorModeSwitcher.tsx
+++ src/ColorModeSwitcher.tsx
@@ -1,15 +1,10 @@
 import * as React from "react"
-import {
-  useColorMode,
-  useColorModeValue,
-  IconButton,
-  IconButtonProps,
-} from "@chakra-ui/react"
+import { useColorMode, useColorModeValue, IconButton, IconButtonProps } from "@chakra-ui/react"
 import { FaMoon, FaSun } from "react-icons/fa"
 
 type ColorModeSwitcherProps = Omit<IconButtonProps, "aria-label">
 
-export const ColorModeSwitcher: React.FC<ColorModeSwitcherProps> = (props) => {
+export const ColorModeSwitcher: React.FC<ColorModeSwitcherProps> = props => {
   const { toggleColorMode } = useColorMode()
   const text = useColorModeValue("dark", "light")
   const SwitchIcon = useColorModeValue(FaMoon, FaSun)
